40f81e40b9b4d5db29747d05e959dbf340560dbc
marikell <marikell_silva@hotmail.com>
2022-10-20 19:28:15 -0300

    Initial Commit

---
+++ src/main/java/br/edu/infnet/appoficina/AppoficinaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appoficina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppoficinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppoficinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/appoficina/AppoficinaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appoficina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppoficinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
